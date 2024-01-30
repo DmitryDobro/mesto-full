b353d86a4e77bd9bc526b441e0ab991f9da89384
Dima Dobronovich <dobronovich_95@mail.ru>
2024-01-30 16:18:40 +0700

    fix

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.dobryi.nomoredomainsmonster.ru';
 
 function checkResponse(res) {
   if (res.ok) {
--- frontend/src/utils/constans.js
+++ frontend/src/utils/constans.js
@@ -1,7 +1,7 @@
 const configApi = {
   url: 
-  // 'https://api.dobryi.nomoredomainsmonster.ru',
-  'http://localhost:3000',
+  'https://api.dobryi.nomoredomainsmonster.ru',
+  // 'http://localhost:3000',
   // url: 'https://nomoreparties.co/v1/cohort-74',
   headers: {
     // authorization: 'ce4643f4-83a7-46c3-a332-ce4ad63e4a8b',
